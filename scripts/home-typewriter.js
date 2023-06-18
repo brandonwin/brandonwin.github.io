8506f74ffb1fbae5e6b57e6bedd362fb121c0906
Brandon WIn <93178071+brnwin@users.noreply.github.com>
2023-06-18 17:09:16 -0400

    🚀 Cleaned up file system and created preview version

--- scripts/home-typewriter.js
+++ scripts/home-typewriter.js
@@ -1,34 +1,14 @@
-var app = document.getElementById('tw-subhead');
+var app = document.getElementById('home-subhead');
 
 var typewriter = new Typewriter(app, {
-  loop: true,
-  deleteSpeed: 5,
+  loop: false,
+  deleteSpeed: 6,
+  delay: 40,
 });
 
 typewriter
-  .pauseFor(1000)
-  .typeString('product designer')
-  .pauseFor(1000)
-  .deleteChars(16)
-  .pauseFor(500)
-  .typeString('people person')
-  .pauseFor(1000)
-  .deleteChars(13)
-  .pauseFor(500)
-  .typeString('UX/UI designer')
-  .pauseFor(1000)
-  .deleteChars(14)
-  .pauseFor(500)
-  .typeString('game designer')
-  .pauseFor(1000)
-  .deleteChars(13)
-  .pauseFor(500)
-  .typeString('Twitch.tv geek')
-  .pauseFor(1000)
-  .deleteChars(14)
-  .pauseFor(500)
-  .typeString('Connect 4 lover')
-  .pauseFor(1000)
-  .deleteChars(20)
-  .pauseFor(500)
+  .pauseFor(300)
+  .typeString('a product designer with a passion for intuitive and accessible design. (•◡•)')
+  .pauseFor(1500)
+  .deleteChars(6)
   .start();
